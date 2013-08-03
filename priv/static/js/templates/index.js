88a59726ca235030c20105dbdad662fbf414447e
Joe Jackson <joe.jackson@scimendsolutions.com>
2013-08-03 10:18:04 -0400

    House Cleaning -- Clean up rendered templates and javascript files

--- priv/static/js/templates/index.js
+++
@@ -1,9 +0,0 @@
-Ember.TEMPLATES["index"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
-this.compilerInfo = [3,'>= 1.0.0-rc.4'];
-helpers = helpers || Ember.Handlebars.helpers; data = data || {};
-  
-
-
-  data.buffer.push("<h2>Videos</h2>\n<section id=\"videos\">\n</section>\n");
-  
-});
